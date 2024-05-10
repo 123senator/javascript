cbb208d037295fd7dd664599f9f4f0725d657f76
Osinachi <osinachidesire@gmail.com>
2024-05-10 12:35:46 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// datatypes in javascript
+// 2 main Datatypes in js which are primitive and reference/ object type
+
+//primitive data types 
+//number. floating call numbers{alway have decimal even though it can't be seen}
+//string
+//boolean
+//underfined
+//null
+//big int
+//symbol.
+
+let num = 23;
+console.log(num);
+let student = "obi";
+let greeting = "what's";
+let g = true;
+let b;
+console.log(b);
+
+//LET
+let age = 20;
+console.log(age);
+age = 21;
+console.log(age);
+
+//CONST
+const birthYear = 1920;
+console.log(birthYear);
+
+//VAR
+var job = "Programmer";
+console.log(job);
+job = "teacher";
+console.log(job);
+
+//Block scope
+{
+    let age = 20;
+    console.log(typeof age);
+}
+{
+    let age = "age";
+    console.log(age);
+}
+
+//
+const numb = "23";
+console.log(typeof numb);
+
+// naming variable
+const last_name = "f";
+const new3 = 33;
+let MYname = "somtoo";
+
+// Arithmettic operators
+console.log(2+4);
+console.log(4-39);
+const year = 2024;
+const ageNigeria = year - 1960;
+console.log("Nigeria age", ageNigeria);
+console.log(year / 10);
+console.log(year * 2);
+
+// Modulus
+console.log(7 % 2);
+console.log(10 % 4);
+let x = 10;
+console.log(++x);
+console.log(x);
+// console.log(x++); "Remember: the ++ sign placed before adds 1 immediately but when placed after you have to run console.log(x) to give the current value of x in the console."
+
