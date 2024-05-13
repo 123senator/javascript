b69d1abeee1b8607b5e68319f8620a170c2b0e70
Osinachi <osinachidesire@gmail.com>
2024-05-13 16:43:35 -0700

    second commit

---
+++ js/assignment.js
@@ -0,0 +1,28 @@
+// let MW1 = 78;
+// let MH1 = 1.69;
+// let JW1 = 92;
+// let JH1 = 1.95;
+
+// let markBMI = MW1 / MH1 **2;
+// let johnBMI = JW1 / JH1 **2;
+
+// let markHigherBMI1 = markBMI > johnBMI1;
+
+// console.log("Mark's BMI1" markBMI1);
+// console.log("John's BMI1" johnBMI1);
+// console.log("Is Mark's BMI higher than John's" markHigherBMI1);
+
+// // second data
+// let MW2 = 95;
+// let MH2 = 1.88;
+// let JW2 = 85;
+// let JH2 = 1.76;
+
+// let markBMI2 = MW2 / MH2 **2;
+// let johnBMI2 = JW2 / JH2 **2;
+
+// let markHigherBMI2 = markBMI2 > johnBMI2;
+
+// console.log("Mark's BMI2" markBMI2);
+// console.log("John's BMI2" johnBMI2);
+// console.log("Is Mark's BMI higher than John's" markHigherBMI2);
--- js/script.js
+++ js/script.js
@@ -79,7 +79,6 @@ x2 += y;
 // x2 = x2+ y ;; x2 = 3 + 4
 console.log(x2);
 console.log(y);
-
 y += x2;
 // y = x2 + y
 console.log(y, x2);
@@ -121,3 +120,35 @@ console.log((23 - 50) * 3 **3);
 // console.log(4 + 5 + 6);
 let j = 4 + 5 + 6;
 console.log((4 + 5 + 6) / 3);
+
+
+
+// Assignment 1
+let MW1 = 78;
+let MH1 = 1.69;
+let JW1 = 92;
+let JH1 = 1.95;
+
+let markBMI1 = MW1 / MH1 **2;
+let johnBMI1 = JW1 / JH1 **2;
+
+let markHigherBMI1 = markBMI1 > johnBMI1;
+
+console.log("Mark's BMI1", markBMI1);
+console.log("John's BMI1", johnBMI1);
+console.log("Is Mark's BMI higher than John's", markHigherBMI1);
+
+// second data
+let MW2 = 95;
+let MH2 = 1.88;
+let JW2 = 85;
+let JH2 = 1.76;
+
+let markBMI2 = MW2 / MH2 **2;
+let johnBMI2 = JW2 / JH2 **2;
+
+let markHigherBMI2 = markBMI2 > johnBMI2;
+
+console.log("Mark's BMI2", markBMI2);
+console.log("John's BMI2", johnBMI2);
+console.log("Is Mark's BMI higher than John's", markHigherBMI2);
